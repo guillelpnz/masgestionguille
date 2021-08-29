9de3cc7ac9a59f87d104355bbbf77586af97c371
Guillermo Lupiáñez Tapia <guillelupianez99@gmail.com>
2021-08-29 19:43:14 +0200

    Update settings.py

--- proyectomasgestion/settings.py
+++ proyectomasgestion/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r!lh4*m_k8z0_qv-!lqcv4t69vefnz8niv_r^_33okh5it0ze^'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
